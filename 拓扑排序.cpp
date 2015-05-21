4af5d6fd123b0a9d3b5538279b0cc480a8855b1c
yadongshao <348829719@qq.com>
2015-05-21 22:41:14 +0800

    Create 拓扑排序.cpp

---
+++ 拓扑排序.cpp
@@ -0,0 +1,77 @@
+//拓扑排序是对于有向无环图的遍历 
+
+#include<iostream>
+#include<algorithm>
+#include<queue>
+#include<cstdio>
+#include<cstring>
+using namespace std;
+const int MAXN = 110;
+const int MAXM = 10010;
+
+int head[MAXN],indegree[MAXM],N,M,T;
+struct EdgeNode
+{
+	int to;
+	int w;
+	int next;
+};
+EdgeNode Edges[MAXM];
+
+
+int toposort()
+{
+	queue<int> Q;
+	int u;
+	int Count = 0;
+	for(int i = 1; i <= N; i++)
+	{
+		if(indegree[i] == 0)
+			Q.push(i);
+		else
+			Count++;
+	}
+	int id = 0;
+	while(!Q.empty())
+	{
+		u = Q.front();
+		Q.pop();
+		for(int i = head[u]; i != -1; i = Edges[i].next)
+		{
+			indegree[Edges[i].to]--;
+			if(indegree[Edges[i].to]==0)
+			{
+				id++;
+				Q.push(Edges[i].to);
+			}
+		}
+	}
+	if(id < Count)
+		return false;
+	else
+		return true;
+}
+int main()
+{
+	int x,y;
+	while(cin >> N >> M)
+	{
+		memset(head,-1,sizeof(head));
+		memset(indegree,0,sizeof(indegree));
+		for(int i = 0; i < M; i++)
+		{
+			cin >> x >> y;
+			Edges[i].to = y;
+			Edges[i].w = 1;
+			Edges[i].next = head[x];
+			head[x] = i;
+			indegree[y]++;
+		}
+		if(toposort())
+			cout << "YES" << endl;
+		else
+			cout << "NO" << endl;
+	}
+
+	return 0;
+}
