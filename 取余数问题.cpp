21ba50a5ddfdcf982a769ddaefe9eedd9c3724c9
yadongshao <348829719@qq.com>
2015-05-21 23:43:41 +0800

    Update 取余数问题.cpp

--- 取余数问题.cpp
+++ 取余数问题.cpp
@@ -9,10 +9,10 @@ int main(){
 	{
 
 		int temp=m%n;
-		temp=temp-1;
+		temp=temp-1;//当为零的时候  后面会出现负数
 		if (temp<0)
 		{
-			temp=temp+n;
+			temp=temp+n;//使用加一个周期的方式进行
 		}
 		cout<<temp<<endl;
 	}
